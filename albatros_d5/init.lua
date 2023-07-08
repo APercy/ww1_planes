b672fbd79a88a506260afac21fdccb9f3fe673f7
Alexsandro Percy <alexsandro.percy@gmail.com>
2023-07-08 19:20:58 -0300

    removed unused parameter

--- albatros_d5/init.lua
+++ albatros_d5/init.lua
@@ -127,7 +127,6 @@ albatros_d5.plane_properties = {
     _seats = {{x=0,y=-1.5,z=-8.89039},},
     _seats_rot = {0},  --necessary when using reversed seats
     _have_copilot = false, --wil use the second position of the _seats list
-    _max_occupants = 1,
     _max_plane_hp = 80,
     _enable_fire_explosion = true,
     _longit_drag_factor = 0.13*0.13,
